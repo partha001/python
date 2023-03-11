745997e6793898a7c20efe2084dda30eacfdb7f9
partha-desktop\partha <biswas.partha1@gmail.com>
2023-03-11 17:32:27 +0530

    decorators

---
+++ pythonBasics/15Decorators/3FunctionsAsReturnValue.py
@@ -0,0 +1,36 @@
+#example1
+def calculator(operation):
+    def add(a,b):
+        return a+b
+    def subtract(a,b):
+        return a-b
+    if operation == "add":
+        return add
+    elif operation == "subtract":
+        return subtract
+    
+print(calculator("add")(10,4)) #op: 14
+print(calculator("subtract")(7,7)) #op: 0
+
+
+#example2: storing functions in lists
+def square(num):
+    return num**2
+
+def cube(num):
+    return num**3
+
+def times10(num):
+    return num**10
+
+
+operations = [square, cube, times10]
+for func in operations:
+    print(func(5))
+## op:
+# 25
+# 125
+# 9765625
+
+
+
