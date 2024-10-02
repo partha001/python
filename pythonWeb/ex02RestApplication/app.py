c877c28fb9bc8f654328fbcb9d43833f461f24e1
Partha Biswas <biswas.partha1@gmail.com>
2024-10-03 02:30:07 +0530

    code cleanup

--- pythonWeb/ex02RestApplication/app.py
+++ pythonWeb/ex02RestApplication/app.py
@@ -2,13 +2,15 @@ import uuid
 
 from flask import Flask, request
 from db import items, stores
+
 app = Flask(__name__)
 
 
 # api to get all store details
 @app.get("/store")
 def get_stores():
-    return {"stores":stores}
+    return {"stores": list(stores.values())}
+
 
 # api to create store
 # note: reading data post request payload
@@ -17,42 +19,50 @@ def get_stores():
 # request payload:  {"name": "MyStore1"}
 @app.post("/store")
 def create_stores():
-    request_data = request.get_json() #reads the post-request payload to a dictionary
+    request_data = request.get_json()  #reads the post-request payload to a dictionary
     storeId = uuid.uuid4().hex
 
     for store in stores:
-        if store["name"] == request_data["name"]:
-            new_store = {"storeId": storeId, "name": request_data["name"],"items":[]}
-            stores.append(new_store)
-            return "store = {} created".format(request_data["name"]), 201
-    return "store = {} already exists".format(request_data["name"]), 200
+        if (stores[store])["name"] == request_data["name"]:
+            return "store = {} already exists".format(request_data["name"]), 200
 
-
-# api to create items within a particular store
-# note: this shows how to read path-variable or path-parama
-# http://localhost:5000/store/MyStore1/item
-# {"name": "Table","price":20}
-@app.post("/store/<string:name>/item")
-def create_item(name):
-    print("requested store name is : {}".format(name))
-    request_data = request.get_json() #reads the post-request payload to a dictionary
-    for store in stores:
-        if store["name"] == name:
-            new_item  = {"name": request_data["name"], "price": request_data["price"]}
-            store["items"].append(new_item)
-            return new_item,201
-    return "store={} not found".format(name), 200
+    new_store = {"storeId": storeId, "name": request_data["name"], "items": []}
+    stores[storeId] = new_store
+    return new_store, 201
 
 
 # api to get details of a particular store
-# note: reading request query parameters
-#  http://localhost:5000/store/getStoreDetails?storeId=100
+#  http://localhost:5000/store/getStoreDetails?storeId=ae4445f6545546e3b51a0cacd0b4c525
 @app.get("/store/getStoreDetails")
 def getStoreDetails():
     storeId = request.args.get('storeId')
     if storeId in stores:
-        return stores.get(storeId) , 200
-    return "store with name = {} not found".format(storeId), 200
+        return stores.get(storeId), 200
+    return "store with storeId = {} not found".format(storeId), 200
 
 
+# api to create items within a particular store
+# http://localhost:5000/item
+# {"name": "Table","price":20, "storeId":"100"}
+@app.post("/item")
+def create_item():
+    item_data = request.get_json()
+    if item_data["storeId"] not in stores:
+        return "storeId:{} is not found ".format(item_data["storeId"]), 404
 
+    itemId = uuid.uuid4().hex
+    item = {**item_data, "itemId": itemId}
+    items[itemId] = item
+    return item, 201
+
+
+@app.get("/items")
+def get_all_items():
+    return {"items": list(items.values())}, 200
+
+
+@app.get("/items/<string:itemId>")
+def get_item_with_itemid(itemId):
+    if itemId not in items:
+        return "item with itemId:{} not found".format(itemId)
+    return items[itemId], 200
