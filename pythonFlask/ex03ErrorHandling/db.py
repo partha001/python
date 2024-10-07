0d7d47368987c13d1dfb81f5f4c078af7be1ec1a
partha biswas <biswas.partha1@gmail.com>
2024-10-07 06:16:51 +0530

    error handling and adding new endpoints

---
+++ pythonFlask/ex03ErrorHandling/db.py
@@ -0,0 +1,15 @@
+#decoupling stores and items
+stores = {}
+items ={}
+
+# # the items will have a structure like the below
+# items = {
+#     1: {
+#         "name" : "Table",
+#         "price": 17.99
+#     },
+#     2: {
+#         "name" : "Chair",
+#         "price": 100.99
+#     }
+# }
